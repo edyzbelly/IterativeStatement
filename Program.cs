8d63757f3a3a1da335a7756ed725637ee75e17a8
edyzbelly <77354889+edyzbelly@users.noreply.github.com>
2021-01-29 15:20:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IterativeStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer value between 1 and 100 to execute an iterative statement:");
+            
+            try
+            {
+                string input = Console.ReadLine();
+                int value = int.Parse(input);
+
+                int i;
+                for(i = 0; i < value; i++)
+                { 
+                    Console.WriteLine("You have entered" + value.ToString() + ". This is the current integer value " +
+                        "in the loop: " + i.ToString()); 
+                }
+               
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer.");
+            }
+        }
+    }
+}
