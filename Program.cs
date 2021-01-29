8e411fbb25d0b734b4f5dbca86b10d586c365a8a
edyzbelly <77354889+edyzbelly@users.noreply.github.com>
2021-01-29 15:38:08 -0500

    input validation

--- Program.cs
+++ Program.cs
@@ -13,17 +13,27 @@ namespace IterativeStatement
                 string input = Console.ReadLine();
                 int value = int.Parse(input);
 
-                int i;
-                for(i = 1; i <= value; i++)
-                { 
-                    Console.WriteLine("You have entered " + value.ToString() + ". This is the current integer value " +
-                        "in the loop: " + i.ToString()); 
+                if ((value >= 1) && (value <= 100))
+                {
+                    int i;
+                    for (i = 1; i <= value; i++)
+                    {
+                        Console.WriteLine("You have entered " + value.ToString() + ". This is the current integer value " +
+                        "in the loop: " + i.ToString());
+                    }
                 }
-               
+                else
+                {
+                    Console.WriteLine("Please enter an integer between 1 and 100 and try again ...");
+                    Console.WriteLine("Press any key to exit the program and try again ...");
+                    Console.ReadKey(true);
+                }
             }
             catch
             {
                 Console.WriteLine("Please enter an integer.");
+                Console.WriteLine("Press any key to exit the program and try again ...");
+                Console.ReadKey(true);
             }
         }
     }
